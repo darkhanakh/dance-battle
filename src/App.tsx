8711e1fca01d291c54ab2ed5cad12a1017378a76
Darkhan <darkhanahmetov228@gmail.com>
2023-06-29 15:48:25 +0600

    feat: add simple hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 function App() {
   return (
-    <div>
-      <div className="container">
-        <h1>Лучшие танцоры n!</h1>
-        <p>Танцуй с любимыми менторами нашего дорогого инкубатора</p>
-        <button>Начать игру</button>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-lg">
+          <h1 className="text-5xl font-bold">Лучшие танцоры n!</h1>
+          <p className="py-6">
+            Лучшая танцевальная игра с нашими любими менторами, которая позволит
+            нам окунуться в захватывающий мир ритма и движения. Эта игра
+            предлагает уникальный опыт, объединяющий яркую графику,
+            захватывающие треки и наших неподражаемых менторов.
+          </p>
+          <button className="btn btn-primary">Начни игру</button>
+        </div>
       </div>
     </div>
   );
