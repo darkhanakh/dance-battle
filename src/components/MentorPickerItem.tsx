c48d656fea68a61533b81fed5a5d49998a81b856
Darkhan <darkhanahmetov228@gmail.com>
2023-06-29 22:36:41 +0600

    feat: added mentor picking logic and showing it to user

--- src/components/MentorPickerItem.tsx
+++ src/components/MentorPickerItem.tsx
@@ -2,11 +2,23 @@ import React from "react";
 
 interface MentorPickerItemProps {
   src: string;
+  onSelect: () => void;
+  disabled: boolean;
+  selected: boolean;
 }
 
-const MentorPickerItem: React.FC<MentorPickerItemProps> = ({ src }) => {
+const MentorPickerItem: React.FC<MentorPickerItemProps> = ({
+  src,
+  onSelect,
+  disabled,
+  selected,
+}) => {
   return (
-    <button className="avatar join-item">
+    <button
+      className="avatar join-item"
+      onClick={onSelect}
+      disabled={disabled || selected}
+    >
       <div className="w-36 rounded">
         <img src={src} alt="mentor" />
       </div>
--- src/pages/MentorSelection.tsx
+++ src/pages/MentorSelection.tsx
@@ -1,24 +1,110 @@
-import React from "react";
+import React, { useState } from "react";
 import MentorPickerItem from "../components/MentorPickerItem.tsx";
+import data from "../../data.json";
+
+interface PlayersMentor {
+  name: string;
+  src: string;
+}
+
+interface SelectedMentors {
+  player1: PlayersMentor | null;
+  player2: PlayersMentor | null;
+}
+
+type CurrentPlayer = "player1" | "player2";
 
 const MentorSelection: React.FC = () => {
+  const mentors: PlayersMentor[] = data;
+
+  const [selectedMentors, setSelectedMentors] = useState<SelectedMentors>({
+    player1: null,
+    player2: null,
+  });
+  const [currentPlayer, setCurrentPlayer] = useState<CurrentPlayer>("player1");
+
+  const handleMentorSelection = (mentor: { name: string }) => {
+    if (
+      selectedMentors.player1?.name === mentor.name ||
+      selectedMentors.player2?.name === mentor.name
+    ) {
+      return;
+    }
+
+    setSelectedMentors((prevSelectedMentors) => ({
+      ...prevSelectedMentors,
+      [currentPlayer]: mentor,
+    }));
+
+    if (currentPlayer === "player1") {
+      setCurrentPlayer("player2");
+    }
+  };
+
+  const renderPlayer = (player: "player1" | "player2") =>
+    player === "player1" ? "Первый игрок" : "Второй игрок";
+
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="flex items-center justify-between">
-        <div className="first-dancer"></div>
-        <div className="flex flex-col justify-center items-center">
-          <h1 className="text-2xl mb-3">Выбери своего ментора</h1>
-          <div className="join mb-3">
-            <MentorPickerItem src="../../public/mentor_1.jpg" />
-            <MentorPickerItem src="../../public/mentor_2.jpg" />
-            <MentorPickerItem src="../../public/mentor_3.jpg" />
-            <MentorPickerItem src="../../public/mentor_4.jpg" />
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <div className="flex items-center justify-between">
+          <div className="first-dancer max-w-md card">
+            {selectedMentors.player1 && (
+              <>
+                <h2 className="text-center mb-1 text-2xl">
+                  Выбор первого игрока
+                </h2>
+                <img
+                  className="w-fit h-96"
+                  src={selectedMentors.player1.src}
+                  alt="Selected Mentor"
+                />
+              </>
+            )}
           </div>
-          <button className="btn btn-primary">Начать игру</button>
+          <div className="flex flex-col justify-center items-center mx-5">
+            <h1 className="text-primary text-4xl mb-3">
+              Выбери своего ментора
+            </h1>
+            <div className="join mb-3 flex-wrap max-w-xl">
+              {mentors.map((mentor) => (
+                <MentorPickerItem
+                  key={mentor.name}
+                  src={mentor.src}
+                  onSelect={() => handleMentorSelection(mentor)}
+                  disabled={selectedMentors[currentPlayer] !== null}
+                  selected={
+                    selectedMentors.player1 === mentor ||
+                    selectedMentors.player2 === mentor
+                  }
+                />
+              ))}
+            </div>
+            {selectedMentors.player1 && selectedMentors.player2 ? (
+              <button className="btn btn-primary">Начать игру</button>
+            ) : (
+              <p className=" text-xl">
+                {renderPlayer(currentPlayer)} - Выбери ментора
+              </p>
+            )}
+          </div>
+          <div className="second-dancer max-w-md card">
+            {selectedMentors.player2 && (
+              <>
+                <h2 className="text-center mb-1 text-2xl">
+                  Выбор второго игрока
+                </h2>
+                <img
+                  className="w-fit h-96 max-w-fit"
+                  src={selectedMentors.player2.src}
+                  alt="Selected Mentor"
+                />
+              </>
+            )}
+          </div>
         </div>
-        <div className="second-dancer"></div>
       </div>
-    </div>
+    </>
   );
 };
 
